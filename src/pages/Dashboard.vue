865d4d3fa820ed6910a3124deaacd815e2a6f2ee
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2020-06-26 17:33:47 -0400

    ADD: fist dashboard component

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-xs-12 col-sm-12 col-md-6">
+        <dashRecepcion></dashRecepcion>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6">
+        <dashRecepcion></dashRecepcion>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import dashRecepcion from '../components/dashRecepcion.vue'
+export default {
+  components: {
+    dashRecepcion
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page>
+    INDEX
+  </q-page>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
