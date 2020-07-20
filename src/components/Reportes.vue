37583a03039495874aacecc16a0bb34a2acab07c
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2020-07-20 17:25:39 -0400

    ADD: date filter to table reports

--- src/components/Reportes.vue
+++ src/components/Reportes.vue
@@ -10,16 +10,18 @@
       :filter="filter"
       @request="loadTableData"
       binary-state-sort
+      :visible-columns="visibleColumns"
+      dense
     >
       <template v-slot:top>
         <div class="q-pa-md">
           <div class="row q-col-gutter-md">
-            <div class="col-12 q-table__title">Recepciones por periodo  <q-btn class=" float-right" round color="secondary" icon="search" /></div>
+            <div class="col-12 q-table__title">Recepciones por periodo  <q-btn class=" float-right" round color="secondary" icon="search" @click="searchFilter" /></div>
             <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12" >
-              <q-datetime-picker mode="date" label="Desde" v-model="dateFrom"></q-datetime-picker>
+              <q-datetime-picker clearable auto-update-value mode="date" label="Desde" v-model="dateFrom" ></q-datetime-picker >
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12" >
-              <q-datetime-picker mode="date" label="Hasta" v-model="dateTo"></q-datetime-picker>
+              <q-datetime-picker clearable auto-update-value mode="date" label="Hasta" v-model="dateTo" ></q-datetime-picker >
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
               <q-select
@@ -61,6 +63,8 @@
 // import IEcharts from 'vue-echarts-v3/src/full.js'
 import dashRecepcion from '../components/dashRecepcion.vue'
 import dashProduccion from '../components/dashProduccion.vue'
+import { date } from 'quasar'
+
 export default {
   name: 'reportes',
   components: {
@@ -105,32 +109,39 @@ export default {
     }
   }, */
   methods: {
+    searchFilter () {
+      this.onRequest({
+        pagination: this.pagination,
+        filter: undefined
+      })
+    },
     onRequest (props) {
       // eslint-disable-next-line no-unused-vars
       const { page, rowsPerPage, sortBy, descending } = props.pagination
       // eslint-disable-next-line no-unused-vars
       const filter = props.filter
-
+      console.log(filter)
       this.loading = true
       this.loadTableData(props)
     },
-    dateOptionsB (date) {
-      return date >= '2019/02/03' && date <= '2020/12/15'
-    },
     loadTableData (props) {
       console.log(props)
+      var df = new Date(this.dateFrom)
+      var dt = new Date(this.dateTo)
+      var dateFrom = date.formatDate(df, 'YYYY-MM-DD')
+      var dateTo = date.formatDate(dt, 'YYYY-MM-DD')
+      var filter = { from: dateFrom, to: dateTo }
       this.data = []
-      console.log(props)
       // eslint-disable-next-line no-unused-vars
       const { page, rowsPerPage, sortBy, descending } = props.pagination
       // eslint-disable-next-line no-unused-vars
-      const filter = props.filter
       this.loading = true
       this.$store.dispatch('reports/getTableData', {
         page,
         rowsPerPage,
         sortBy,
-        descending
+        descending,
+        filter
       }).then(response => {
         /* DATA */
         response.data.data.forEach((value, index) => {
@@ -139,7 +150,7 @@ export default {
         this.loading = false
         this.select_disable = false
         this.pagination.sortBy = sortBy
-        this.pagination.descending = !this.pagination.descending
+        // this.pagination.descending = this.pagination.descending
         this.pagination.page = response.data.current_page
         this.pagination.rowsPerPage = response.data.per_page
         this.pagination.rowsNumber = response.data.total
@@ -157,13 +168,13 @@ export default {
         })
         this.visibleColumns.push('tipo_Mov', 'Lote', 'fecha_cosecha', 'peso_neto', 'ESTADO')
         this.loading = false
-        // this.prevPage = response.data.
-        // this.nextPage =
       })
     },
     filterFn (val, update) {
+      // console.log(val === '')
       if (val === '') {
         update(() => {
+          console.log(update)
           this.options = this.columns
 
           // with Quasar v1.7.4+
@@ -177,22 +188,6 @@ export default {
         const needle = val.toLowerCase()
         this.options = this.columns.filter(v => v.toLowerCase().indexOf(needle) > -1)
       })
-    },
-    onScroll ({ to, ref }) {
-      console.log('scroll')
-      /* const lastIndex = this.data.length - 1
-
-      if (this.loading !== true && this.nextPage < this.lastPage && to === lastIndex) {
-        this.loading = true
-
-        setTimeout(() => {
-          this.nextPage++
-          this.$nextTick(() => {
-            ref.refresh()
-            this.loading = false
-          })
-        }, 500)
-      } */
     }
   }
 }
